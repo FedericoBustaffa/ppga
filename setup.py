37f6d40da01908025a95a785924b88b5261677e9
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-14 16:37:14 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ppga",
     version="0.0.3",
-    package_dir={"": "."},
-    packages=find_packages(where="."),
+    packages=find_packages(),
     url="https://github.com/FedericoBustaffa/ppga",
     author="FedericoBustaffa",
     install_requires=["psutil", "colorama", "psutil"],
