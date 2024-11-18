7b3f6b0e0ffae2225f3c650c6dadab0ed7bb6080
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-19 00:07:27 +0100

    analysis

--- benchmarks/analysis.py
+++ benchmarks/analysis.py
@@ -1,5 +1,17 @@
-import numpy as np
+import os
 import sys
+
+import numpy as np
+
+
+def read_file() -> list[str]:
+    file = open("logs/benchmark.log", "r")
+    lines = file.readlines()
+    lines.sort(key=lambda x: x.split(" ")[-3])
+    file.close()
+
+    return lines
+
 
 def sequential_analysis():
     times = {
@@ -11,6 +23,18 @@ def sequential_analysis():
         "replacement": [],
         "stime": [],
     }
+
+    lines = read_file()
+    for line in lines:
+        words = line.split(" ")
+        key = words[-3].removesuffix(":")
+        times[key].append(float(line.split(" ")[-2]))
+
+    for key in times.keys():
+        print(f"total {key} time: {np.sum(times[key])} s")
+        print(f"mean {key} time: {np.mean(times[key]) * 1000.0} ms")
+        print("-" * 50)
+
 
 def parallel_analysis():
     times = {
@@ -24,15 +48,7 @@ def parallel_analysis():
         "parallel": [],
     }
 
-
-def main(argv: list[str]):
-
-    file = open("logs/benchmark.log", "r")
-    lines = file.readlines()
-    file.close()
-
-    lines.sort(key=lambda x: x.split(" ")[-3])
-
+    lines = read_file()
     for line in lines:
         words = line.split(" ")
         key = words[-3].removesuffix(":")
@@ -42,6 +58,11 @@ def main(argv: list[str]):
         print(f"total {key} time: {np.sum(times[key])} s")
         print(f"mean {key} time: {np.mean(times[key]) * 1000.0} ms")
         print("-" * 50)
+
+
+def main(argv: list[str]):
+    sequential_analysis()
+    # parallel_analysis()
 
 
 if __name__ == "__main__":
