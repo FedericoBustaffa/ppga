3e61b35b6c41437fe925d113567b8ed299c36785
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-20 22:37:44 +0100

    speed up

--- benchmarks/analysis.py
+++ benchmarks/analysis.py
@@ -3,8 +3,6 @@ import os
 import sys
 
 import pandas as pd
-import parallel
-import sequential
 
 
 def read_file(filepath: str) -> list[dict]:
@@ -36,13 +34,11 @@ def main(argv: list[str]):
         os.mkdir("results")
 
     # sequential simulation
-    sequential.main(argv)
     lines = read_file("logs/sequential.json")
     stats = parse_values(lines)
     stats.to_csv("results/sequential.csv", header=True, index=False)
 
     # parallel simulation
-    parallel.main(argv)
     lines = read_file("logs/parallel.json")
     pstats = parse_values(lines)
     pstats.to_csv("results/parallel.csv", header=True, index=False)
@@ -50,9 +46,42 @@ def main(argv: list[str]):
     stime = stats[stats["field"] == "stime"]["time"].sum()
     ptime = pstats[pstats["field"] == "ptime"]["time"].sum()
 
+    # total time
     print(f"sequential time: {stime} seconds")
     print(f"parallel time: {ptime} seconds")
     print(f"speed up: {stime / ptime}")
+
+    # take only the parallelized part
+    cx_time = stats[stats["field"] == "crossover"]["time"].sum()
+    mut_time = stats[stats["field"] == "mutation"]["time"].sum()
+    eval_time = stats[stats["field"] == "evaluation"]["time"].sum()
+    print(f"sequential cx + mut + eval time: {cx_time + mut_time + eval_time} seconds")
+
+    # sync + work time
+    parallel_time = pstats[pstats["field"] == "parallel"]["time"].sum()
+    print(f"in parallel time: {parallel_time} seconds")
+
+    # extract the worst worker time
+    df = (
+        pstats[pstats["process_name"] != "MainProcess"]
+        .groupby(["process_name", "field"])
+        .sum()
+        .groupby("process_name")
+        .sum()
+    )
+
+    worst_worker = df["time"].max()
+    best_worker = df["time"].min()
+
+    print(f"worst worker: {worst_worker} seconds")
+    print(f"best worker: {best_worker} seconds")
+
+    # approximate sync time
+    print(f"sync time: {parallel_time - worst_worker} seconds")
+
+    # pure speed up
+    cx_mut_eval = cx_time + mut_time + eval_time
+    print(f"pure speed up: {cx_mut_eval / parallel_time}")
 
 
 if __name__ == "__main__":
