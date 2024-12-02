c82b6a33ee7eabb6ae756b37f58df932a2adef98
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-12-03 00:26:45 +0100

    mem

--- ppga/parallel/worker.py
+++ ppga/parallel/worker.py
@@ -1,5 +1,4 @@
 import multiprocessing as mp
-import multiprocessing.queues as mpq
 from multiprocessing.shared_memory import SharedMemory
 
 import numpy as np
@@ -7,48 +6,20 @@ import numpy as np
 from ppga import log
 
 
-def compute(
-    send_q: mpq.Queue,
-    recv_q: mpq.Queue,
-):
+def compute(shape, dtype, index, offset, toolbox, cxpb, mutbp):
     logger = log.getCoreLogger()
     logger.debug("start")
-    mem = None
+    mem = SharedMemory(name="couples", create=False)
+    chunk = np.ndarray(shape=shape, dtype=dtype, buffer=mem.buf)
 
-    while True:
-        task = send_q.get()
-        if task is None:
-            logger.debug("received termination chunk")
-            break
-
-        func, index, offset, shape, dtype, args, kwargs = task
-        if mem is None:
-            mem = SharedMemory(name="couples", create=False)
-        chunk = np.ndarray(shape=shape, dtype=dtype, buffer=mem.buf)
-        recv_q.put(func(chunk[index:offset], *args, **kwargs))
-
-    assert isinstance(mem, SharedMemory)
+    del chunk
     mem.close()
     logger.debug("terminated")
 
 
 class Worker(mp.Process):
     def __init__(self) -> None:
-        self.send_q = mp.Queue()
-        self.recv_q = mp.Queue()
-
-        super().__init__(
-            target=compute,
-            args=[self.send_q, self.recv_q],
-        )
-
-    def send(self, chunk) -> None:
-        self.send_q.put(chunk)
-
-    def recv(self):
-        result = self.recv_q.get()
-        return result
+        super().__init__(target=compute)
 
     def join(self, timeout: float | None = None) -> None:
-        self.send_q.put(None)
         super().join(timeout)
